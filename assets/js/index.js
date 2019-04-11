5a982b45c558cf77f2c9b3bb9591768695e9a238
tingting <tingtinglin159@gmail.com>
2019-04-11 10:00:38 +0800

    110519032

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+import"../css/fonts/PT-Sans-Bold.eot";
+import"../css/fonts/PT-Sans-Bold.svg";
+import"../css/fonts/PT-Sans-Bold.ttf";
+import"../css/fonts/PT-Sans-Bold.woff";
+import"../css/fonts/PT-Sans-BoldItalic.eot";
+import"../css/fonts/PT-Sans-BoldItalic.svg";
+import"../css/fonts/PT-Sans-BoldItalic.ttf";
+import"../css/fonts/PT-Sans-BoldItalic.woff";
+import"../css/fonts/PT-Sans-Italic.eot";
+import"../css/fonts/PT-Sans-Italic.svg";
+import"../css/fonts/PT-Sans-Italic.ttf";
+import"../css/fonts/PT-Sans-Italic.woff";
+import"../css/fonts/PT-Sans.eot";
+import"../css/fonts/PT-Sans.svg";
+import"../css/fonts/PT-Sans.ttf";
+import"../css/fonts/PT-Sans.woff";
+import $ from 'jquery';
+import 'bootstrap';
+import '../css/style.css';
+import bb from '../images/bird.jpg';
+import ss from '../images/squirrel.jpg';
+
+$('#bird').attr("src",bb);
+$('#squirrel').attr("src",ss);
+
+$(document).ready(function () {
+	// when a hanburger menu is clicked
+	let $ul = $(".nav__list"),
+		 $menu = $(".nav__menu");
+
+	$($menu).click(function () {
+		// toggle menu-click Class
+		$ul.toggleClass("menu-click");
+	}); // end click event handler
+});
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+let path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry:'./assets/js/index.js',
+    output:{
+        path:path.join(__dirname,'./dist'),
+        filename:'bundle.[chunkhash].js',
+        //publicPath:'../dist/'
+    },
+    module:{
+        rules:[{
+            test:/\.(js)$/,
+            exclude:/(node_modules)/,
+            use:{
+                loader:'babel-loader',
+                options:{
+                    presets:['@babel/preset-env']
+                }
+            }
+        },
+        {
+            
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: [{
+                   loader: 'css-loader',
+                   options: {
+                      url: false
+                   }
+                }, {
+                   loader: 'postcss-loader'
+                }]
+             })
+            
+         },
+        {
+            test:/\.(jpe?g|png|gif|svg)$/,
+            use:[{
+                loader:'url-loader',
+                options:{
+                    limit:40000,
+                    outputPath:'./images'
+                }
+                },
+                'image-webpack-loader'
+            ]
+        },
+        {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            use:{
+                loader:'file-loader',
+                options: {
+                    outputPath: 'css/fonts',
+                    name: '[name].[ext]',
+                },
+            }
+        }
+    ]
+    },
+    plugins:[
+        new ExtractTextPlugin('./css/style.css'),
+        new HtmlWebpackPlugin({
+            template:'assets/index.html'
+            
+        })
+    ]
+}
